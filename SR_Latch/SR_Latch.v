65d0fbc712f7e49b1ca84cb5fd76312ab546173b
Siddhapura-Yash <siddhapurayash09@gmail.com>
2025-03-29 00:36:42 +0530

    Create SR_Latch.v

---
+++ SR_Latch/SR_Latch.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module SR_Latch(S,R,Q,Qbar);
+input S,R;
+output reg Q,Qbar;
+
+always@(*)
+begin
+case ({S,R})
+2'b00 : begin Q = Q;  Qbar = Qbar; end
+2'b01 : begin Q = 0;  Qbar = 1; end
+2'b10 : begin Q = 1;  Qbar = 0; end
+2'b11 : begin Q = 1'bX;  Qbar = 1'bX; end
+endcase
+end
+
+endmodule
